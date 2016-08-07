531b2d4d72f16b204776e8a3c72a0f64f13cb92a
Vishwanathan Iyer <vsiyer91@gmail.com>
2016-08-07 18:26:38 +0530

    modified genFile2 to make partB from partA and changed markes in partB file

--- HW1-Bash/genFile2.sh
+++ HW1-Bash/genFile2.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-echo "RollNum,Marks" > part2.csv
-pass=20
-for i in $( tail -n +2 $1) 
-do
+#Generates PartB
+#echo "RollNum,Name,partB" > $2
+awk -F"," '{
+	if(NR!=1)
+	{
 		
-       	if [$( cut -d',' -f2 $i)  -gt $pass ]
-	then
-	  	echo $i >> part2.csv
+		if($3-i >= 20.0)  
+		{
+			print $1 "," $2 "," $3 
+			
+		}			
+	}
+}' $1 >> $2
 
-	fi
-        #echo  $i | cut -d',' -f2
-        #echo 
-done
-
+cat $2
